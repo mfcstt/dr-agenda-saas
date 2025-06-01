90b24f2d9d96a8b275bfdb21e88434b6c2f09c55
Maria Fernanda Rizzo <mahcosta54@gmail.com>
2025-06-01 13:09:28 -0300

    feat: ensure doctors are listed on doctors page

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,17 +1,22 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import {
   PageActions,
   PageContainer,
+  PageContent,
   PageDescription,
   PageHeader,
   PageHeaderContent,
   PageTitle,
 } from "@/src/components/ui/page-container";
+import { db } from "@/src/db";
+import { doctorsTable } from "@/src/db/schema";
 import { auth } from "@/src/lib/auth";
 
 import AddDoctorButton from "./_components/add-doctor-button";
+import DoctorCard from "./_components/doctor-card";
 
 const DoctorsPage = async () => {
   const session = await auth.api.getSession({
@@ -25,24 +30,31 @@ const DoctorsPage = async () => {
       redirect("/dashboard");
     }
 
+    const doctors = await db.query.doctorsTable.findMany({
+      where: eq(doctorsTable.clinicId, session.user.clinic.id),
+    });
+
     return (
-      <div>
-        <PageContainer>
-          <PageHeader>
-            <PageHeaderContent>
-              <PageTitle>Médicos</PageTitle>
-              <PageDescription>
-                Gerencie os médicos de sua clínica.
-              </PageDescription>
-            </PageHeaderContent>
-            <PageActions>
-              <AddDoctorButton />
-            </PageActions>
-          </PageHeader>
-        </PageContainer>
-      </div>
-    );
-  }
+     <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <AddDoctorButton />
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <div className="grid grid-cols-3 gap-6">
+          {doctors.map((doctor) => (
+            <DoctorCard key={doctor.id} doctor={doctor} />
+          ))}
+        </div>
+      </PageContent>
+    </PageContainer>
+  );
 };
+}
 
 export default DoctorsPage;
